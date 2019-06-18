8670ec5407fbfeb91065d6ebf899854bb7e335fe
Lê Công Tráng <45307867+tranglc@users.noreply.github.com>
2019-06-18 20:36:05 +0700

    Cập nhật thêm đồ thị thời gian thực

--- script-python-linux.py
+++ script-python-linux.py
@@ -1,25 +1,52 @@
 #!/usr/bin/env python3
-
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import bluetooth
 import time
+
 
 #BLE Comunication
 bd_addr ="00:21:13:05:E1:45"
 port = 1
+
 sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM )
 sock.connect((bd_addr,port))
 
-sock.send("a")
-time.sleep(.20)
-sock.send("b")
-time.sleep(.20)
+# Create figure for plotting
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+xs = []
+ys = []
+# This function is called periodically from FuncAnimation
+def animate(i, xs, ys):
 
-# Display Lux
-while True:
-	sock.send("d")
+    # Read Lux from BH1750
 	time.sleep(.02)
 	sock.send("e")
 	time.sleep(.02)
-	lux=str(sock.recv(10))
+	lux=int(sock.recv(10))
 	time.sleep(.1)
-	print("Gia tri Lux: " + lux[2:8])
+	print(lux)
+
+	# Add x and y to lists
+	xs.append(dt.datetime.now().strftime('%H:%M:%S,%m/%d/%Y'))
+	ys.append(lux)
+
+	# Limit x and y lists to 20 items
+	xs = xs[-20:]
+	ys = ys[-20:]
+
+	# Draw x and y lists
+	ax.clear()
+	ax.plot(xs, ys,'-o',alpha=0.8)
+	# Format plot
+	plt.style.use('ggplot')
+	plt.xticks(rotation=45, ha='right')
+	plt.subplots_adjust(bottom=0.30)
+	plt.title('Light Intensity:'+' '+str(lux)+' (lux) at '+dt.datetime.now().strftime('%H:%M:%S,%m/%d/%Y'))
+	plt.ylabel('Light Intensity (LUX)')
+
+# Set up plot to call animate() function periodically
+ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=1000)
+plt.show()
